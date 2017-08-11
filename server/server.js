3a03f6bf92fb96f72e10b20042c0afea1023e1a0
Stuart Terrell <sterrell6@cox.net>
2017-08-11 07:27:50 -0700

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -20,30 +22,50 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
-      
   });
 });
-// GET /todos/12222
+
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
-     if (!ObjectID.isValid(id)) {
+
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-    Todo.findById(id).then((todo) => {
-        if (!todo){
-             return res.status(404).send();
-            }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
     res.send({todo});
-    }).catch((e) => {
-        res.status(400).send();
-    });
-    
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
